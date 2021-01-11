17cc8b79bf5d7dda7e7dce710420dc7c41b163f4
kalitawojciech <wojtek1311997@gmail.com>
2021-01-11 21:43:02 +0100

    rename Form1 into Menu

--- MuPlusLambdaAlgorithm/Menu.cs
+++ MuPlusLambdaAlgorithm/Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MuPlusLambdaAlgorithm
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
